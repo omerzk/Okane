8b364458d118d6175ca9920e3e758239f4dc94ef
Omer Zaks <omerzaks@OMacBook-Pro.lan>
2025-08-28 23:30:15 +0300

    init

---
+++ Okane/ShufersalMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShufersalMoneyApp.swift
+//  ShufersalMoney
+//
+//  Created by Omer Zaks on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ShufersalMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
